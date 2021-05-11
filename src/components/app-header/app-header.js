82242764ca27aeadddb22170d08d3cf44f5834fa
asentsov <asentsov@yandex-team.ru>
2021-05-12 01:35:41 +0300

    init commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css'
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Alex S</h1>
+            <h2>5 records 0 likes</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
